ee426815b6f262fa988b330b0730c1609008a0cb
Yevhenii Smolenko <Jacksonsmolenko@gmail.com>
2021-04-05 22:24:05 +0200

    added api

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+    res.send("" + ch(4))
+})
+
+app.listen(3000)
+
 const ch = function(x) {
-    Math.cosh(4)
+    return Math.cosh(x)
 }
 
 module.exports = ch
