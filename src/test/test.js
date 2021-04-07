2f8cd99f07261d8c9822213d00a6b0c5191ebe70
Yevhenii Smolenko <Jacksonsmolenko@gmail.com>
2021-04-07 20:50:24 +0200

    test fix

--- src/test/test.js
+++ src/test/test.js
@@ -1,11 +1,11 @@
-const ch = require('../app/index')
+const chA = require('../app/index')
 var assert = require('assert');
 
 
 describe('index', function() {
-    describe('#ch', function() {
+    describe('#chA', function() {
         it('should return 27.308232836016487', function() {
-            assert.equal(ch(4), 27.308232836016487);
+            assert.equal(chA(4), 27.308232836016487);
         });
     });
 });
